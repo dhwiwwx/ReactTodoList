6ecdc0a4ed6d3b5e5f62031fd010f00b64e0d99d
dhwiwwx <dhwiwwx@naver.com>
2022-09-23 19:45:44 +0900

    TodoHead 완성

--- react-todolist/src/App.js
+++ react-todolist/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {createGlobalStyle} from 'styled-components';
-import TodoTemplate from './components/TodoTemplate'
+import TodoTemplate from './components/TodoTemplate';
+import TodoHead from "./components/TodoHead";
 
 const GlobalStyle = createGlobalStyle`
 body {
@@ -13,7 +14,9 @@ function App() {
   return (
     <>
     <GlobalStyle/>
-    <TodoTemplate>안녕하세요</TodoTemplate>
+    <TodoTemplate>
+      <TodoHead/> 
+    </TodoTemplate>
     </>
   );
 }
